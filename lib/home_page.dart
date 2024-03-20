c46f459a05cb50dfa63bd22f3a741f4881d0b16e
Muhammad Aditya Rahman <145524126+adityarahman47@users.noreply.github.com>
2024-03-20 15:44:46 +0800

    Update home_page.dart (menambah  elevated button)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -69,3 +69,13 @@ class HomePage extends StatelessWidget {
               style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
             ),
             const Icon(Icons.favorite, size: 50.0, color: Colors.blue),
+
+            const SizedBox(height: 20.0),
+            const Text(
+              '6. ElevatedButton:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Ini ElevatedButton'),
+            ),
